11bb4ea1e763ba29e12eb0800c231420e4c6f793
Jameel Evans <jameelevans@Jameels-iMac.lan>
2023-03-01 13:24:14 -0700

    Added new contact form

--- page-contact.php
+++ page-contact.php
@@ -14,7 +14,8 @@
       
 
       <div id="contact__top" class="contact__main">
-      <iframe width="100%" height="1900px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=e7mQz0a-AEqXAIHOT_G39naZkcCiRUhFppTpmJtkTglUOUJONURQQThJRVdXUVRKU0dIUDhHUE01NC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      <iframe width="100%" height="1900px" src="https://forms.microsoft.com/Pages/ResponsePage.aspx?id=e7mQz0a-AEqXAIHOT_G39icgaPASScRDkICn5z52dZlURElKVE5aQlBGUEk4VjFXMjZGVTNWOENZNiQlQCN0PWcu&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      <!--<iframe width="100%" height="1900px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=e7mQz0a-AEqXAIHOT_G39naZkcCiRUhFppTpmJtkTglUOUJONURQQThJRVdXUVRKU0dIUDhHUE01NC4u&embed=true" frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>-->
       </div>
       
     </section>
