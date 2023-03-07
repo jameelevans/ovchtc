30bd8ffe79e118e291ab20711708b81e3d681120
Jameel Evans <jameelevans@Jameels-iMac.lan>
2023-03-07 15:06:32 -0700

    Removed FAQs section on FAQs page

--- page-ovc-faqs.php
+++ page-ovc-faqs.php
@@ -16,13 +16,14 @@
             <?php the_content();?>
           </div>
       </div>
+      <!--
       <div class="hr">&nbsp;</div>
 
       <div id="faqs__top" class="faq__main">
        
         
         <div class="faqs" role="tablist" aria-live="polite" data-behavior="accordion">
-          <?php
+          <?php/*
             $faqs = new WP_Query(array(
               'posts_per_page' => -1,
               'post_type' => 'faqs',
@@ -32,15 +33,15 @@
               if($faqs->have_posts()) {
                       while($faqs->have_posts()) {
                               $faqs->the_post();?>
-                              <!-- Faqs Category -->
+                            
                                 <?php
                                 $get_cat        = get_the_category();
                                 $first_cat      = $get_cat[0];
                                 $category_name  = $first_cat->cat_name;
                                 $category_link  = get_category_link( $first_cat->cat_ID ); ?>
                                 <h2 class="faqs__heading">Topic - <?php echo  $category_name; ?></h2>
-                                <!-- .Faqs Category -->
-                                <!-- Faqs -->
+                               
+                             
                                 <article class="faqs__item js-show-item-default" data-binding="expand-accordion-item" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
                                   <span id="tab-<?php echo get_the_ID();?>" tabindex="0" class="faqs__title" aria-controls="panel-<?php echo get_the_ID();?>" role="tab" aria-selected="false" aria-expanded="false" data-binding="expand-accordion-trigger">
                                     <h5 itemprop="name"><?php the_title();?></h5>
@@ -55,7 +56,7 @@
                                 </article>
                             
                             
-                          <!-- .FAQs -->
+                        
                       <?php }
                               } else { ?>
                               <p class="faqs__no-show">There are no FAQs to show yet</p>
@@ -65,11 +66,11 @@
         </div>
         
         <div class="faqs__sidebar">
-          <div class="faqs__sort"><?php sort_webinar_posts();?></div>
+          <div class="faqs__sort"><?php sort_webinar_posts();*/?></div>
 
           
         </div>
-      </div>
+      </div>-->
 
       
       
